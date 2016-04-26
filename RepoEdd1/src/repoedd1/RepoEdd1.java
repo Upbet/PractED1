94e0b3d579ca57bddf4e82919c22dbd91f1ff71b
Upbet <registermain@gmail.com>
2016-04-26 12:40:53 +0200

    Modificando desde eclipse

--- RepoEdd1/src/repoedd1/RepoEdd1.java
+++ RepoEdd1/src/repoedd1/RepoEdd1.java
@@ -15,7 +15,9 @@ public class RepoEdd1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    	System.out.print("Hola Mundo");
+    	System.out.println("IDE Eclipse");
     }
     
 }
